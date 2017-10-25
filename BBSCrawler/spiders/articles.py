e93f0c2d83e39edaf2d6a3b2859618fd07bed035
ouyangsong <songouyang@live.com>
2017-10-25 15:05:35 +0800

    articles 抓取启动分布式

--- BBSCrawler/spiders/articles.py
+++ BBSCrawler/spiders/articles.py
@@ -9,32 +9,19 @@
 import scrapy
 from BBSCrawler.items import ArticlesItem
 import json
-from pymongo import MongoClient
+from scrapy_redis.spiders import RedisCrawlSpider
 
 
-class ArticlesSpider(scrapy.Spider):
+class ArticlesSpider(RedisCrawlSpider):
     # 主要抓取各个板块的文章列表
     name = "articles"
     allowed_domains = ["bbs.byr.cn"]
-
-    def start_requests(self):
-        client = MongoClient(self.settings["MONGO_HOST"], self.settings["MONGO_PORT"])
-        db = client[self.settings["MONGO_DATABASE"]]
-        db.authenticate(self.settings["MONGO_USER"], self.settings["MONGO_PASSWORD"])
-        boards = db["section"]
-        for item in boards.find({}, {"board.name": 1}):
-            for i in item["board"]:
-                yield scrapy.Request("https://bbs.byr.cn/open/board/{board}.json?&oauth_token={oauth_token}"
-                                     "&page={page_num}".format(board=i["name"],
-                                                               oauth_token=self.settings["OAUTH_TOKEN"], page_num=1),
-                                     meta={'board': i["name"], 'page_num': 1},
-                                     callback=self.parse)
+    redis_key = 'articles:start_urls'
 
     def parse(self, response):
         dict_response = json.loads(response.body, encoding='utf-8')
-        item = ArticlesItem()
-
         for article in dict_response["article"]:
+            item = ArticlesItem()
             item["id"] = article["id"]
             item["group_id"] = article["group_id"]
             item["reply_id"] = article["reply_id"]
@@ -52,11 +39,13 @@ class ArticlesSpider(scrapy.Spider):
             item["reply_count"] = article["reply_count"]
             item["last_reply_user_id"] = article["last_reply_user_id"]
             item["last_reply_time"] = article["last_reply_time"]
+            self.logger.info(item["id"])
             yield item
 
         if dict_response["pagination"]["page_current_count"] < dict_response["pagination"]["page_all_count"]:
-            board = response.meta["board"]
+            board = dict_response["name"]
             page_num = dict_response["pagination"]["page_current_count"] + 1
+            self.logger.info("board: {board}, pagenum: {page_num}".format(board=board, page_num=page_num))
             yield scrapy.Request("https://bbs.byr.cn/open/board/{board}.json?&oauth_token={oauth_token}"
                                  "&page={page_num}".format(board=board,
                                                            oauth_token=self.settings["OAUTH_TOKEN"], page_num=page_num),
